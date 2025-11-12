3fc13b3dde1f07407e421e8f5a375b967a508826
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-11-12 23:19:55 +0700

    create transaction entity

---
+++ internal/domain/transaction/entity.go
@@ -0,0 +1,21 @@
+package transaction
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
+	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key;" json:"id"`
+	MerchantID  uuid.UUID `gorm:"type:uuid;not null" json:"merchant_id"`
+	OrderID     string    `gorm:"type:size:100;not null" json:"order_id"`
+	Provider    string    `gorm:"type:size:100;not null" json:"provider"`
+	Amount      float64   `gorm:"type:numeric;not null" json:"amount"`
+	Status      string    `gorm:"type:size:50;not null" json:"status"`
+	ExternalRef string    `gorm:"type:size:100;not null" json:"external_ref"`
+	RedirectURL string    `gorm:"type:size:255;not null" json:"redirect_url"`
+	RawResponse string    `gorm:"type:jsonb;not null" json:"raw_response"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
