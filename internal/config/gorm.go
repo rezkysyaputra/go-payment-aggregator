c1278ed3dff25d3dc155864cc0348bc2a946ebd2
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-12-02 21:33:59 +0700

    feat: Establish initial project structure by relocating API spec and adding core configurations, server entrypoint, transaction service, and routing.

---
+++ internal/config/gorm.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewDatabse(viper *viper.Viper, log *logrus.Logger) *gorm.DB {
+	// load config
+	username := viper.GetString("database.username")
+	password := viper.GetString("database.password")
+	host := viper.GetString("database.host")
+	port := viper.GetInt("database.port")
+	database := viper.GetString("database.name")
+	maxIdleConnection := viper.GetInt("database.pool.idle")
+	maxConnection := viper.GetInt("database.pool.max")
+	maxLifetimeConnection := viper.GetInt("database.pool.lifetime")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai", host, username, password, database, port)
+
+	// open connection
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.New(&logrusWriter{Logger: log}, logger.Config{
+			SlowThreshold:             time.Second * 5,
+			Colorful:                  false,
+			IgnoreRecordNotFoundError: true,
+			ParameterizedQueries:      true,
+			LogLevel:                  logger.Info,
+		}),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to connect databse: %v", err)
+	}
+
+	connection, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to connect databse: %v", err)
+	}
+
+	// set connection pool
+	connection.SetConnMaxIdleTime(time.Duration(maxIdleConnection))
+	connection.SetMaxOpenConns(maxConnection)
+	connection.SetConnMaxLifetime(time.Duration(maxLifetimeConnection))
+
+	return db
+}
+
+type logrusWriter struct {
+	Logger *logrus.Logger
+}
+
+func (l *logrusWriter) Printf(message string, args ...any) {
+	l.Logger.Tracef(message, args...)
+}
