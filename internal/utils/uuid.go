dd494f0f0756c4bd38bc72d29d60aefb144d63c3
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-12-27 17:42:10 +0700

    feat: add GenerateApiKey function and UUID generation utility

---
+++ internal/utils/uuid.go
@@ -0,0 +1,11 @@
+package utils
+
+import "github.com/google/uuid"
+
+func GenerateUUIDV7() (uuid.UUID, error) {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return id, nil
+}
