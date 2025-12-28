c6547f105c42786e8e43a6f216d0cfbe28c36a42
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-12-28 15:14:01 +0700

    feat: add response handling and RegisterMerchantResponse struct

---
+++ internal/pkg/response/response.go
@@ -0,0 +1,39 @@
+package response
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func Success(c *gin.Context, code int, status string, message string, data interface{}) {
+	c.JSON(code, Response{
+		Code:    code,
+		Status:  status,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func Error(c *gin.Context, code int, status string, message string) {
+	c.JSON(code, Response{
+		Code:    code,
+		Status:  status,
+		Message: message,
+		Data:    nil,
+	})
+}
+
+type RegisterMerchantResponse struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	Status      string `json:"status"`
+	ApiKey      string `json:"api_key"`
+	CallbackURL string `json:"callback_url"`
+}
