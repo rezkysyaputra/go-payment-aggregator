44b1bc116fa2af7c1e2b980e66ff47b851925500
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-12-29 00:03:14 +0700

    feat: add CreatedAt and UpdatedAt fields to RegisterMerchantResponse struct

--- internal/pkg/response/response.go
+++ internal/pkg/response/response.go
@@ -32,12 +32,14 @@ func Error(c *gin.Context, code int, status string, message string) {
 }
 
 type RegisterMerchantResponse struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	Status      string `json:"status"`
-	ApiKey      string `json:"api_key"`
-	CallbackURL string `json:"callback_url"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Email       string    `json:"email"`
+	Status      string    `json:"status"`
+	ApiKey      string    `json:"api_key"`
+	CallbackURL string    `json:"callback_url"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type GetMerchantResponse struct {
