2d7e68acd436cd91144113e7fc4a3c86ee13431e
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-12-30 00:36:29 +0700

    feat: add Redis client initialization in the test package

--- test/init.go
+++ test/init.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/redis/go-redis/v9"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
@@ -20,12 +21,15 @@ var log *logrus.Logger
 
 var validate *validator.Validate
 
+var redisClient *redis.Client
+
 func init() {
 	viperConfig = config.NewViper()
 	log = logrus.New()
 	validate = validator.New()
 	app = gin.Default()
 	db = config.NewDatabase(viperConfig, log)
+	redisClient = config.NewRedis(viperConfig, log)
 
 	config.Bootstrap(&config.BootstrapConfig{
 		Config:   viperConfig,
@@ -33,5 +37,6 @@ func init() {
 		App:      app,
 		Log:      log,
 		Validate: validate,
+		Redis:    redisClient,
 	})
 }
