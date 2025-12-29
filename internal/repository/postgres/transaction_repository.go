88151f68a7d1ef87698c9445d469f027e7b8e882
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2025-12-30 00:03:51 +0700

    feat: update TransactionModel to include default values for Amount and Status fields

--- internal/repository/postgres/transaction_repository.go
+++ internal/repository/postgres/transaction_repository.go
@@ -1,9 +1,12 @@
 package postgres
 
 import (
+	"context"
+	"go-payment-aggregator/internal/domain"
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type TransactionModel struct {
@@ -12,9 +15,9 @@ type TransactionModel struct {
 	OrderID       string    `gorm:"size:255;not null;unique"`
 	Provider      string    `gorm:"size:255"`
 	PaymentMethod string    `gorm:"size:255"`
-	Amount        int64     `gorm:"not null"`
+	Amount        int64     `gorm:"default:0;not null"`
 	Currency      string    `gorm:"size:10;not null;default:'IDR'"`
-	Status        string    `gorm:"size:50;not null"`
+	Status        string    `gorm:"size:50;not null;default:'PENDING'"`
 	ExternalRef   string    `gorm:"size:255;not null"`
 	RedirectURL   string    `gorm:"size:255"`
 	RawResponse   []byte    `gorm:"type:jsonb"`
@@ -25,4 +28,62 @@ type TransactionModel struct {
 
 func (TransactionModel) TableName() string {
 	return "transactions"
+}
+
+// toTransactionModel converts domain.Transaction to TransactionModel
+func toTransactionModel(tx *domain.Transaction) *TransactionModel {
+	return &TransactionModel{
+		ID:            tx.ID,
+		MerchantID:    tx.MerchantID,
+		OrderID:       tx.OrderID,
+		Provider:      tx.Provider,
+		Amount:        tx.Amount,
+		Currency:      tx.Currency,
+		Status:        string(tx.Status),
+		ExternalRef:   tx.ExternalID,
+		PaymentMethod: tx.PaymentMethod,
+		RedirectURL:   tx.PaymentURL,
+		RawResponse:   []byte(tx.RawResponse),
+		ExpiredAt:     tx.ExpiredAt,
+		CreatedAt:     tx.CreatedAt,
+		UpdatedAt:     tx.UpdatedAt,
+	}
+}
+
+// toDomain converts TransactionModel to domain.Transaction
+func (t *TransactionModel) toDomain() *domain.Transaction {
+	return &domain.Transaction{
+		ID:            t.ID,
+		MerchantID:    t.MerchantID,
+		OrderID:       t.OrderID,
+		Provider:      t.Provider,
+		Amount:        t.Amount,
+		Currency:      t.Currency,
+		Status:        domain.TransactionStatus(t.Status),
+		ExternalID:    t.ExternalRef,
+		PaymentMethod: t.PaymentMethod,
+		PaymentURL:    t.RedirectURL,
+		RawResponse:   string(t.RawResponse),
+		ExpiredAt:     t.ExpiredAt,
+		CreatedAt:     t.CreatedAt,
+		UpdatedAt:     t.UpdatedAt,
+	}
+}
+
+type transactionRepository struct {
+	db *gorm.DB
+}
+
+func NewTransactionRepository(db *gorm.DB) domain.TransactionRepository {
+	return &transactionRepository{
+		db: db,
+	}
+}
+
+func (t *transactionRepository) Create(ctx context.Context, tx *domain.Transaction) (*domain.Transaction, error) {
+	model := toTransactionModel(tx)
+	if err := t.db.WithContext(ctx).Create(model).Error; err != nil {
+		return nil, err
+	}
+	return model.toDomain(), nil
 }
