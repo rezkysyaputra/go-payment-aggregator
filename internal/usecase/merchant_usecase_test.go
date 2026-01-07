c8e74bfe3c0eaffc2abdcc0072c0c523dcdeb3f1
rezkysyaputra <muhrezkysyaputra100@gmail.com>
2026-01-07 17:32:01 +0700

    test: enhance GetProfile tests and add UpdateProfile tests in MerchantUsecase

--- internal/usecase/merchant_usecase_test.go
+++ internal/usecase/merchant_usecase_test.go
@@ -100,14 +100,14 @@ func TestMerchantUsecase_GetProfile(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "Success Get Merchant",
+			name: "Success Get Merchant Profile",
 			mock: func(repo *mocks.MockMerchantRepository) {
 				repo.On("FindByID", mock.Anything, merchantID).Return(returnedMerchant, nil)
 			},
 			wantErr: false,
 		},
 		{
-			name: "Failed Get Merchant - Not Found",
+			name: "Failed Get Merchant Profile - Not Found",
 			mock: func(repo *mocks.MockMerchantRepository) {
 				repo.On("FindByID", mock.Anything, merchantID).Return(nil, errors.New("Merchant Not Found"))
 			},
@@ -135,6 +135,80 @@ func TestMerchantUsecase_GetProfile(t *testing.T) {
 				assert.Equal(t, returnedMerchant.Name, res.Name)
 				assert.Equal(t, returnedMerchant.Balance, res.Balance)
 			}
+		})
+	}
+}
+
+func TestMerchantUsecase_UpdateProfile(t *testing.T) {
+	reqUC := &domain.UpdateMerchantRequest{
+		Name:        "Test Merchant",
+		CallbackURL: "https://example.com/callback",
+	}
+
+	merchantID := pkg.GenerateUUIDV7()
+	returnedMerchant := &domain.Merchant{
+		ID:          merchantID,
+		Name:        reqUC.Name,
+		Email:       "merchant@example.com",
+		CallbackURL: reqUC.CallbackURL,
+		ApiKey:      "merchant-api-key",
+		Status:      domain.MerchantStatusActive,
+		Balance:     0,
+	}
+
+	tests := []struct {
+		name    string
+		mock    func(repo *mocks.MockMerchantRepository)
+		wantErr bool
+	}{
+		{
+			name: "Success Update Merchant Profile",
+			mock: func(repo *mocks.MockMerchantRepository) {
+				repo.On("FindByID", mock.Anything, merchantID).Return(returnedMerchant, nil)
+
+				repo.On("Update", mock.Anything, mock.AnythingOfType("*domain.Merchant")).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "Failed Repository Update Merchant Profile - Not Found",
+			mock: func(repo *mocks.MockMerchantRepository) {
+				repo.On("FindByID", mock.Anything, merchantID).Return(nil, errors.New("Merchant Not Found"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "Failed Repository Update Merchant Profile - Bad Request",
+			mock: func(repo *mocks.MockMerchantRepository) {
+				repo.On("FindByID", mock.Anything, merchantID).Return(returnedMerchant, nil)
+
+				repo.On("Update", mock.Anything, mock.AnythingOfType("*domain.Merchant")).Return(errors.New("Bad Request"))
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(mocks.MockMerchantRepository)
+
+			tt.mock(mockRepo)
+
+			merchantUC := usecase.NewMerchantUC(mockRepo, time.Second*2)
+
+			ctx := context.Background()
+			res, err := merchantUC.UpdateProfile(ctx, merchantID, reqUC)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+				assert.Equal(t, returnedMerchant.Name, res.Name)
+				assert.NotEmpty(t, res.ApiKey)
+			}
+
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
